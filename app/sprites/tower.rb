76ffccc2f4459cd5abae7709ed64bce2f59b9d96
Ahmet <ahmet@kyan.com>
2014-01-30 13:33:27 +0000

    Draw circle around towers (buggy atm)

--- app/sprites/tower.rb
+++ app/sprites/tower.rb
@@ -2,10 +2,17 @@ class Tower < Joybox::Core::Sprite
 
   def initialize(opts={})
     @position = opts[:position]
-    @attack_range = 70
+    @attack_range = 50
     @damage = 10
     @fire_rate = 1.0
     super file_name: 'tower.png', position: @position
+  end
+
+  def draw
+    super.tap do
+      ccDrawColor4B(255, 255, 255, 255)
+      ccDrawCircle(@position, @attack_range, 360, 30, true)
+    end
   end
 
 end
